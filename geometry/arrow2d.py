3681c45203059f0e91356617169e03526d34321f
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-03-18 03:01:34 +0700

    feat: :art: bổ sung hình mũi tên

---
+++ geometry/arrow2d.py
@@ -0,0 +1,54 @@
+import numpy as np
+import OpenGL.GL as GL
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from libs.shader import Shader
+from libs.buffer import VAO, UManager
+from libs.lighting import LightingManager
+
+
+class Arrow:
+    def __init__(self, vert_shader, frag_shader):
+        self.vert_shader = vert_shader
+        self.frag_shader = frag_shader
+        self.vertices = self._generate_arrow()
+        self.colors = np.random.rand(len(self.vertices), 3).astype(np.float32)
+
+        self.vao = VAO()
+        self.shader = Shader(vert_shader, frag_shader)
+        self.uma = UManager(self.shader)
+        self.lighting = LightingManager(self.uma)
+
+    def _generate_arrow(self):
+        vertices = np.array([
+            [-0.5, -0.2, 0.0],
+            [0.3, -0.2, 0.0],
+            [-0.5, 0.2, 0.0],
+            [0.3, 0.2, 0.0],
+            
+            [0.3, -0.4, 0.0],
+            [0.8, 0.0, 0.0],
+            [0.3, 0.4, 0.0],
+        ], dtype=np.float32)
+
+        return vertices
+
+    def setup(self):
+        self.vao.add_vbo(0, self.vertices, ncomponents=3, dtype=GL.GL_FLOAT, normalized=False, stride=0, offset=None)
+        self.vao.add_vbo(1, self.colors, ncomponents=3, dtype=GL.GL_FLOAT, normalized=False, stride=0, offset=None)
+        return self
+
+    def draw(self, projection, view, model):
+        GL.glUseProgram(self.shader.render_idx)
+        modelview = view @ (model if model is not None else np.identity(4, dtype=np.float32))
+        self.uma.upload_uniform_matrix4fv(projection, 'projection', True)
+        self.uma.upload_uniform_matrix4fv(modelview, 'modelview', True)
+        self.vao.activate()
+        
+        GL.glDrawArrays(GL.GL_TRIANGLE_STRIP, 0, 4)
+        GL.glDrawArrays(GL.GL_TRIANGLES, 4, 3)
+        
+        self.vao.deactivate()
